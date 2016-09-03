c5599434f0a67d957bcd2e9399792f72105f5115
Svenyarsh <Sven-yarsh@users.noreply.github.com>
2016-09-03 12:55:39 +0100

    1

---
+++ github_test2/github_test2.cpp
@@ -0,0 +1,11 @@
+// github_test2.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
